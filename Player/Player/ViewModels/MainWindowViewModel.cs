206a045304fbd147a6d39aaab38bcd09e6d3a13d
Philipp Dreßen <philipp@drehsen.net>
2017-03-01 17:24:14 +0100

    Added MainWindowViewModel.cs.

---
+++ Player/Player/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public string Title { get; set; }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
