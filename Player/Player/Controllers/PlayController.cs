43efd2d59feb829b9b51a03e45fc5dd13d2d4204
Philipp Dreßen <philipp@drehsen.net>
2017-03-03 14:50:39 +0100

    Added MediaPlayer to Controller.

--- Player/Player/Controllers/PlayController.cs
+++ Player/Player/Controllers/PlayController.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Player.Controllers
 {
     class PlayController
     {
+        private MediaPlayer mediaplayer = new MediaPlayer();
+
+        public void LoadFile()
+        {
+
+        }
     }
 }
